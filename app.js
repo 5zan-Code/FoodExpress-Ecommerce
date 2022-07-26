052e47f5b2f2aba1092fbbca67fa0a4722251f50
f97aizan <f97aizan@gamil.com>
2022-07-26 16:19:12 -0400

    Pushing Website Project

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var bodyparser = require("body-parser");
+var flash = require("connect-flash");
+var methodoverride = require("method-override");
+var passport = require("passport");
+const dotenv = require('dotenv')
+var localstrategy = require("passport-local");
+var user = require("./models/user");
+var seeddb = require("./seeds");
+var foodroute = require("./routes/foods");
+var commentroute = require("./routes/comment");
+var cartroute = require("./routes/cart");
+var indexroute = require("./routes/index");
+var Port = 3000;
+
+dotenv.config()
+
+mongoose
+  .connect(
+    process.env.URL,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log("error", err.message);
+  });
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+mongoose.set("useFindAndModify", false);
+
+app.use(express.static(__dirname + "/public"));
+
+// seeddb();
+app.use(methodoverride("_method"));
+app.use(flash());
+
+app.use(
+  require("express-session")({
+    secret: "my name is iqbal",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localstrategy(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentuser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
+app.use(indexroute);
+app.use(cartroute);
+app.use(commentroute);
+app.use(foodroute);
+// app.use(foodroute);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening from * ${process.env.PORT}`);
+});
+
+// app.listen(process.env.PORT, process.env.IP,function(){
+//     console.log("food app server has started");
+// });
